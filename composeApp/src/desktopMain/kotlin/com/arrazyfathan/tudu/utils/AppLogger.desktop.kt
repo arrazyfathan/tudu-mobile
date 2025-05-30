8b45e7ff16b451f112a091583f1e7b98367f2ee8
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-05-30 10:28:30 +0700

    update implementation logging desktop

--- composeApp/src/desktopMain/kotlin/com/arrazyfathan/tudu/utils/AppLogger.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/arrazyfathan/tudu/utils/AppLogger.desktop.kt
@@ -1,27 +1,36 @@
 package com.arrazyfathan.tudu.utils
 
+import java.util.logging.Level
+import java.util.logging.Logger
+
 actual object AppLogger {
-    actual fun d(tag: String, message: String) {
-        println("DEBUG: [$tag] $message")
-    }
-
-    actual fun e(tag: String, message: String, throwable: Throwable?) {
-        if (throwable != null) {
-            println("ERROR: [$tag] $message. Throwable: $throwable CAUSE ${throwable.cause}")
-        } else {
-            println("ERROR: [$tag] $message")
-        }
-    }
-
-    actual fun i(tag: String, message: String) {
-        println("INFO: [$tag] $message")
-    }
+    private val logger: Logger = Logger.getLogger(AppLogger::class.java.name)
 
     actual fun v(tag: String, message: String) {
-        println("VERBOSE: [$tag] $message")
+        logger.info("VERBOSE: [$tag] $message")
     }
 
     actual fun w(tag: String, message: String) {
-        println("WARN: [$tag] $message")
+        logger.warning("WARNING: [$tag] $message")
+    }
+
+    actual fun e(
+        tag: String,
+        message: String,
+        throwable: Throwable?
+    ) {
+        if (throwable != null) {
+            logger.log(Level.SEVERE, "ERROR: [$tag] $message", throwable)
+        } else {
+            logger.severe("ERROR: [$tag] $message")
+        }
+    }
+
+    actual fun d(tag: String, message: String) {
+        logger.info("DEBUG: [$tag] $message")
+    }
+
+    actual fun i(tag: String, message: String) {
+        logger.info("INFO: [$tag] $message")
     }
 }
