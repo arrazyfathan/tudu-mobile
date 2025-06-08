b43dd155a71ab87a99fdbb884f66b04ad80ae11f
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-06-08 21:04:38 +0700

    Refactor navigation setup into NavigationRoot

--- composeApp/src/commonMain/kotlin/com/arrazyfathan/tudu/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/arrazyfathan/tudu/app/App.kt
@@ -2,11 +2,7 @@ package com.arrazyfathan.tudu.app
 
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import com.arrazyfathan.tudu.onboarding.presentation.OnboardingScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -14,16 +10,6 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
     MaterialTheme {
         val navController = rememberNavController()
-        NavHost(
-            navController = navController, startDestination = Routes.OnboardingGraph
-        ) {
-            navigation<Routes.OnboardingGraph>(
-                startDestination = Routes.Onboarding
-            ) {
-                composable<Routes.Onboarding> {
-                    OnboardingScreen()
-                }
-            }
-        }
+        NavigationRoot(navController)
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/arrazyfathan/tudu/app/NavigationRoot.kt
@@ -0,0 +1,33 @@
+package com.arrazyfathan.tudu.app
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.arrazyfathan.tudu.onboarding.presentation.OnboardingScreen
+
+@Composable
+fun NavigationRoot(
+    navController: NavHostController,
+) {
+    NavHost(
+        navController = navController, startDestination = Routes.OnboardingGraph
+    ) {
+        onboardingGraph(navController)
+    }
+}
+
+private fun NavGraphBuilder.onboardingGraph(
+    navController: NavHostController
+) {
+    navigation<Routes.OnboardingGraph>(
+        startDestination = Routes.Onboarding
+    ) {
+        composable<Routes.Onboarding> {
+            OnboardingScreen()
+        }
+
+    }
+}
