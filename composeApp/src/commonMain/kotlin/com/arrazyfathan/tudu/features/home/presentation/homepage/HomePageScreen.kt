bb8e0b2938c0247c614d2d56f24bfb39cba5cbc2
arrazyfathan <arrazy.rabbani266@gmail.com>
2025-06-20 14:33:58 +0700

    feat(home): add navigation drawer (#2)

--- composeApp/src/commonMain/kotlin/com/arrazyfathan/tudu/features/home/presentation/homepage/HomePageScreen.kt
+++ composeApp/src/commonMain/kotlin/com/arrazyfathan/tudu/features/home/presentation/homepage/HomePageScreen.kt
@@ -3,10 +3,23 @@ package com.arrazyfathan.tudu.features.home.presentation.homepage
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.arrazyfathan.tudu.core.ui.components.DefaultButton
+import androidx.compose.ui.unit.dp
+import com.arrazyfathan.tudu.core.ui.VerticalSpacer
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -17,16 +30,41 @@ fun HomePageScreen() {
 @Composable
 fun HomePageContent() {
     val viewModel = koinViewModel<HomePageViewModel>()
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        DefaultButton(
-            text = "Logout",
-            onClick = {
-                viewModel.logout()
-            },
-        )
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+        ModalDrawerSheet {
+            Column(
+                modifier =
+                    Modifier
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState()),
+            ) {
+                VerticalSpacer(50.dp)
+                Text("Tudu")
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text("Logout") },
+                    selected = false,
+                    onClick = {
+                        viewModel.logout()
+                    },
+                )
+            }
+        }
+    }) {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+        ) { innerPadding ->
+            Column(
+                modifier = Modifier.fillMaxSize().padding(innerPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text("Home Screen")
+            }
+        }
     }
 }
